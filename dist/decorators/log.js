211824c2c29c19ce9321bac661850bec7cc2b566
preetigaur-sf <preeti.gaur@sourcefuse.com>
2026-03-03 12:24:49 +0530

    Remove dist folder from tracking

--- dist/decorators/log.js
+++
@@ -1,7 +0,0 @@
-export function Log(target, propertyKey, descriptor) {
-    const original = descriptor.value;
-    descriptor.value = function (...args) {
-        console.log(`📘 Method: ${propertyKey}`, args);
-        return original.apply(this, args);
-    };
-}
--- dist/utils/api.js
+++
@@ -1,6 +0,0 @@
-export async function loadBooksFromAPI(url) {
-    const res = await fetch(url);
-    if (!res.ok)
-        throw new Error(`Failed to load ${url}`);
-    return await res.json();
-}
